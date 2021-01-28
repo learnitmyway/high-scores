691185c7c05afe0dee93fd172a1546be04510cbf
David <learnitmyway@users.noreply.github.com>
2021-01-28 22:12:50 +0100

    update test that displays and submits data and then resets it

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
+import axios from "axios";
+
 import App from "./App";
 
-test("displays high score", () => {
+jest.mock("axios");
+
+test("displays and submits data and then resets it", async () => {
   const randomSpy = jest.spyOn(global.Math, "random");
   randomSpy.mockReturnValueOnce(0.9);
   randomSpy.mockReturnValueOnce(1);
@@ -14,5 +19,17 @@ test("displays high score", () => {
   userEvent.click(screen.getByText("generate score"));
   userEvent.click(screen.getByText("generate score"));
 
+  userEvent.type(screen.getByLabelText("Name"), "David");
+
   expect(screen.getByText("score 180")).toBeInTheDocument();
+
+  userEvent.click(screen.getByText("Submit"));
+
+  expect(axios.post).toHaveBeenCalledWith(
+    "api/high-scores",
+    { name: "David", score: 180 },
+    { headers: { "Content-Type": "application/json" } }
+  );
+
+  await waitFor(() => expect(screen.getByText("score 0")).toBeInTheDocument());
 });
