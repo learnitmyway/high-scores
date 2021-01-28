8b6ea039a6bea5c71ca194934d95bf76b4fb1035
David <learnitmyway@users.noreply.github.com>
2021-01-28 21:02:14 +0100

    reformat starter code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const INITIAL_NAME = ''
-const INITIAL_SCORE = 0
-const INITIAL_CLICK_COUNT = 0
+const INITIAL_NAME = "";
+const INITIAL_SCORE = 0;
+const INITIAL_CLICK_COUNT = 0;
 
 function App() {
-  const [name, setName] = useState(INITIAL_NAME)
-  const [score, setScore] = useState(INITIAL_SCORE)
-  const [clickCount, setClickCount] = useState(INITIAL_CLICK_COUNT)
-
+  const [name, setName] = useState(INITIAL_NAME);
+  const [score, setScore] = useState(INITIAL_SCORE);
+  const [clickCount, setClickCount] = useState(INITIAL_CLICK_COUNT);
 
   function handleChange(e) {
-    setName(e.target.value)
+    setName(e.target.value);
   }
 
   function handleScore() {
-    setScore((score) => score + Math.floor((Math.random() * 200) - 100))
-    setClickCount((clickCount) => setClickCount(clickCount + 1))
+    setScore((score) => score + Math.floor(Math.random() * 200 - 100));
+    setClickCount((clickCount) => setClickCount(clickCount + 1));
   }
 
   async function handleSubmit() {
     try {
-      await fetch('https://codesandbox.io/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({name, score})
-    });
+      await fetch("https://codesandbox.io/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, score }),
+      });
     } catch (err) {
-
     } finally {
-      setName(INITIAL_NAME)
-      setScore(INITIAL_SCORE)
-      setClickCount(INITIAL_CLICK_COUNT)
+      setName(INITIAL_NAME);
+      setScore(INITIAL_SCORE);
+      setClickCount(INITIAL_CLICK_COUNT);
     }
   }
 
   return (
     <div className="App">
       <section>
-        <div>
-          click counter {clickCount}
-        </div>
-        <div>
-          score {score}
-        </div>
+        <div>click counter {clickCount}</div>
+        <div>score {score}</div>
         <label>
-          {'Name '}
-         <input value={name} onChange={handleChange}/>
+          {"Name "}
+          <input value={name} onChange={handleChange} />
         </label>
-        <button type="button" onClick={handleSubmit}>Submit</button>
-        <button type="button" onClick={handleScore} disabled={clickCount >= 10}>generate score</button>
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+        <button type="button" onClick={handleScore} disabled={clickCount >= 10}>
+          generate score
+        </button>
       </section>
     </div>
   );
