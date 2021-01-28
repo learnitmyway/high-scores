048f376b036695c0d3dee7d46d849e78756ee9f2
David <learnitmyway@users.noreply.github.com>
2021-01-28 21:34:31 +0100

    test high score is displayed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders", () => {
+test("displays high score", () => {
+  const randomSpy = jest.spyOn(global.Math, "random");
+  randomSpy.mockReturnValueOnce(0.9);
+  randomSpy.mockReturnValueOnce(1);
+
   render(<App />);
-  expect(screen.getByText("Name")).toBeInTheDocument();
+
+  expect(screen.getByText("score 0")).toBeInTheDocument();
+
+  userEvent.click(screen.getByText("generate score"));
+  userEvent.click(screen.getByText("generate score"));
+
+  expect(screen.getByText("score 180")).toBeInTheDocument();
 });
