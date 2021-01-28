a9cf4f9aa68d8156463f0e78225547858fa86a45
David <learnitmyway@users.noreply.github.com>
2021-01-28 22:04:59 +0100

    improve test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,25 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import axios from "axios";
+
+/* eslint-disable no-console, jest/no-standalone-expect */
+console.error = jest.fn();
+
+const spies = {
+  get: jest.spyOn(axios, "get"),
+  patch: jest.spyOn(axios, "patch"),
+  post: jest.spyOn(axios, "post"),
+};
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+afterEach(() => {
+  expect(console.error).not.toHaveBeenCalled();
+
+  expect(spies.get).not.toHaveBeenCalled();
+  expect(spies.patch).not.toHaveBeenCalled();
+  expect(spies.post).not.toHaveBeenCalled();
+});
