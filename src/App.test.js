6c1455dc5c8764be797884be1b11ef726bae9839
David <learnitmyway@users.noreply.github.com>
2021-01-30 13:47:25 +0100

    use describe and it

--- src/App.test.js
+++ src/App.test.js
@@ -7,33 +7,37 @@ import App from "./App";
 
 jest.mock("axios");
 
-test("displays and submits data and then resets it", async () => {
-  const randomSpy = jest.spyOn(global.Math, "random");
-  randomSpy.mockReturnValueOnce(0.9);
-  randomSpy.mockReturnValueOnce(1);
+describe("App", () => {
+  it("displays and submits data and then resets it", async () => {
+    const randomSpy = jest.spyOn(global.Math, "random");
+    randomSpy.mockReturnValueOnce(0.9);
+    randomSpy.mockReturnValueOnce(1);
 
-  render(<App />);
+    render(<App />);
 
-  expect(screen.getByText("score 0")).toBeInTheDocument();
-  expect(screen.getByText("clicks remaining 10")).toBeInTheDocument();
+    expect(screen.getByText("score 0")).toBeInTheDocument();
+    expect(screen.getByText("clicks remaining 10")).toBeInTheDocument();
 
-  userEvent.click(screen.getByText("generate score"));
-  expect(screen.getByText("score 80")).toBeInTheDocument();
-  expect(screen.getByText("clicks remaining 9")).toBeInTheDocument();
+    userEvent.click(screen.getByText("generate score"));
+    expect(screen.getByText("score 80")).toBeInTheDocument();
+    expect(screen.getByText("clicks remaining 9")).toBeInTheDocument();
 
-  userEvent.click(screen.getByText("generate score"));
-  expect(screen.getByText("score 180")).toBeInTheDocument();
-  expect(screen.getByText("clicks remaining 8")).toBeInTheDocument();
+    userEvent.click(screen.getByText("generate score"));
+    expect(screen.getByText("score 180")).toBeInTheDocument();
+    expect(screen.getByText("clicks remaining 8")).toBeInTheDocument();
 
-  userEvent.type(screen.getByLabelText("Name"), "David");
-  userEvent.click(screen.getByText("Submit"));
+    userEvent.type(screen.getByLabelText("Name"), "David");
+    userEvent.click(screen.getByText("Submit"));
 
-  expect(axios.post).toHaveBeenCalledWith(
-    "api/high-scores",
-    { name: "David", score: 180, clicks: 2 },
-    { headers: { "Content-Type": "application/json" } }
-  );
+    expect(axios.post).toHaveBeenCalledWith(
+      "api/high-scores",
+      { name: "David", score: 180, clicks: 2 },
+      { headers: { "Content-Type": "application/json" } }
+    );
 
-  await waitFor(() => expect(screen.getByText("score 0")).toBeInTheDocument());
-  expect(screen.getByText("clicks remaining 10")).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByText("score 0")).toBeInTheDocument()
+    );
+    expect(screen.getByText("clicks remaining 10")).toBeInTheDocument();
+  });
 });
