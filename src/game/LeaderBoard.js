f24731673e567d0b172f5ade1d0b4673006ae9e7
David <learnitmyway@users.noreply.github.com>
2021-01-31 18:54:28 +0100

    improve responsiveness

--- src/game/LeaderBoard.js
+++ src/game/LeaderBoard.js
@@ -2,32 +2,34 @@ import classes from "./LeaderBoard.module.css";
 
 function LeaderBoard({ scores }) {
   return (
-    <table className={classes.root}>
-      <thead className={classes.row}>
-        <tr className={classes.row}>
-          <th>Name</th>
-          <th>Total Points</th>
-          <th>Clicks</th>
-          <th>Average Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        {scores.map((entry, i) =>
-          i < 10 ? (
-            <tr
-              className={classes.row}
-              data-testid="leaderBoardEntry"
-              key={entry.id}
-            >
-              <td>{entry.name}</td>
-              <td>{entry.totalPoints}</td>
-              <td>{entry.clicks}</td>
-              <td>{entry.averagePoints}</td>
-            </tr>
-          ) : null
-        )}
-      </tbody>
-    </table>
+    <div className={classes.root}>
+      <table className={classes.table}>
+        <thead className={classes.row}>
+          <tr className={classes.row}>
+            <th>Name</th>
+            <th>Total Points</th>
+            <th>Clicks</th>
+            <th>Average Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scores.map((entry, i) =>
+            i < 10 ? (
+              <tr
+                className={classes.row}
+                data-testid="leaderBoardEntry"
+                key={entry.id}
+              >
+                <td>{entry.name}</td>
+                <td>{entry.totalPoints}</td>
+                <td>{entry.clicks}</td>
+                <td>{entry.averagePoints}</td>
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- src/game/SubmitScore.js
+++ src/game/SubmitScore.js
@@ -4,20 +4,22 @@ import classes from "./SubmitScore.module.css";
 function SubmitScore({ name, onChange, onSubmit, isSubmitError }) {
   return (
     <section className={classes.root}>
-      <label className={classes.label} htmlFor="submit-score-input">
-        Name
-      </label>
-      <input
-        id="submit-score-input"
-        className={classes.input}
-        value={name}
-        onChange={onChange}
-        placeholder="NAME"
-      />
-      <Button className={classes.button} onClick={onSubmit} text="Save" />
-      <span style={{ color: "red" }}>
-        {isSubmitError && " Error: cannot submit score"}
-      </span>
+      <div>
+        <label className={classes.label} htmlFor="submit-score-input">
+          Name
+        </label>
+        <input
+          id="submit-score-input"
+          className={classes.input}
+          value={name}
+          onChange={onChange}
+          placeholder="NAME"
+        />
+        <Button className={classes.button} onClick={onSubmit} text="Save" />
+        <span style={{ color: "red" }}>
+          {isSubmitError && " Error: cannot submit score"}
+        </span>
+      </div>
     </section>
   );
 }
