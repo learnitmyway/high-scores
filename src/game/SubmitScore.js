cb06d8a5465630b716783ae929b544c21f52206e
David <learnitmyway@users.noreply.github.com>
2021-01-31 17:33:57 +0100

    style input

--- src/game/SubmitScore.js
+++ src/game/SubmitScore.js
@@ -4,10 +4,16 @@ import classes from "./SubmitScore.module.css";
 function SubmitScore({ name, onChange, onSubmit, isSubmitError }) {
   return (
     <section>
-      <label>
-        {"Name "}
-        <input value={name} onChange={onChange} />
+      <label className={classes.label} htmlFor="submit-score-input">
+        Name
       </label>
+      <input
+        id="submit-score-input"
+        className={classes.input}
+        value={name}
+        onChange={onChange}
+        placeholder="NAME"
+      />
       <Button className={classes.button} onClick={onSubmit} text="Send it!" />
       <span style={{ color: "red" }}>
         {isSubmitError && " Error: cannot submit score"}
